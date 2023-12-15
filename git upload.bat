d2c0ffdf0491bc6231dfb024e950e09677f1c2f4
Fab <42010375+fabdeveloper@users.noreply.github.com>
2023-12-15 14:31:19 -0500

    Update on Fr 1/15/_231431

--- git upload.bat
+++ git upload.bat
@@ -1,4 +1,16 @@
+
+REM Get the current date and time
+set "datetime=%DATE% %TIME%"
+set "datetime=%datetime:~0,2%%datetime:~3,2%%datetime:~6,4%_%datetime:~12,2%%datetime:~15,2%%datetime:~18,2%"
+
+REM Add all changes to the staging area
 git add .
-git commit -m "update"
+
+REM Commit the changes with the current date and time
+git commit -m "Update on %datetime%"
+
+REM Push the changes to the remote repository
 git push origin
+
 Pause
+
